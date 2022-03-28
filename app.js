8b243addf1a9ce52eb03533dc480ea40d8aa5584
ashish031 <ab.ashishbhatia31@gmail.com>
2022-03-28 16:48:24 +0530

    second commit

--- app.js
+++ app.js
@@ -6,31 +6,48 @@ new Vue({
         monsterHealth: 100,
         logs: []
     },
-    computed: {
-        winner: function () {
-            if (this.health2 < 0)
-                alert("You have defeated the monster!!");
-            giveUp();
-        }
-    },
     methods: {
-        startGame: function () {
+        startGame() {
             this.gameStatus = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
         },
-        Attack: function () {
-            console.log("called");
-            this.playerHealth -= 5;
-            this.monsterHealth -= 7;
-            return {
+        attack() {
+            let max = 10;
+            let min = 3;
+            let damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
 
+            if (this.monsterHealth <= 0) {
+                alert("You won!!");
+                this.gameStatus = false;
+                return;
+            }
+
+            max = 12;
+            min = 5;
+            damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
+
+            if (this.playerHealth <= 0) {
+                alert("You lost!!");
+                this.gameStatus = false;
+                return;
             }
         },
-        giveUp: function () {
+        specialAttack() {
+
+        },
+        heal() {
+
+        },
+        giveUp() {
             this.gameStatus = !this.gameStatus;
             this.health1 = 100;
             this.health2 = 100;
+        },
+        calculateDamage(min, max) {
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
         }
     }
 })
